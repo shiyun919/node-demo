ba8acbd92c0c29ee00a7d9f4784a456fae8b8299
duyu <c_duoyun@outlook.com>
2019-04-24 16:51:21 +0800

    commit

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-    myButton.addEventListener('click', (e)=>{
+    
+
+
+
+
+//封装前的AJAX请求与响应
+ myButton.addEventListener('click', (e)=>{
 	let request = new XMLHttpRequest() //声明XMLHttpRequest对象
 	request.onreadystatechange = ()=>{
 	 if(request.readyState === 4){
@@ -25,3 +31,49 @@
 	request.open('GET','/xxx')   //配置request的请求类型及路径
 	request.send()	
     })
+
+
+
+
+
+
+
+//封装后的AJAX请求与响应
+
+window.jQuery = function(){}
+
+
+window.$ = window.jQuery
+
+window.jQuery.ajax = function(options){
+	let url = options.url
+	let method = options.method
+	let body = options.body
+	let successFn = options.successFn
+	let failFn = options.failFn
+	
+	let request = new XMLHttpRequest()
+	request.open(method, url)
+	request.onreadystatechange = function(){
+	    if(request.readyState === 4){
+                if(request.status >= 200 && request.status < 300){
+	           successFn.call(undefined, request.responseText)
+	    }else if(request.status >= 400){
+	           failFn.call(undefined, request)
+	    }
+	    
+	  }
+	}
+        request.send(body)
+}
+
+
+myButton.addEventListener('click', (e)=>{
+     window.jQuery.ajax({
+     	url:'/xxx',
+	    method:'get',
+	    successFn: function(x){console.log(x)},
+	    failFn:function(x){console.log(x)}     
+     })
+
+})
