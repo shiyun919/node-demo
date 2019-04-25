7a7f191ee9e5aa465906e3651b5403719fe99154
duyu <c_duoyun@outlook.com>
2019-04-25 11:25:59 +0800

    commit

--- main.js
+++ main.js
@@ -1,85 +1,57 @@
-    
 
 
-
-
-//封装前的AJAX请求与响应
- //myButton.addEventListener('click', (e)=>{
-//	let request = new XMLHttpRequest() //声明XMLHttpRequest对象
-//	request.onreadystatechange = ()=>{
-//	 if(request.readyState === 4){
-//		 console.log('请求响应都完毕了')
-//	    if(request.status >= 200 && request.status < 300){
-//		    console.log('说明请求成功')
-//		    console.log(typeof request.responseText)
-//		    console.log(request.responseText)
-//		    let string = request.responseText
-		    //把符合JSON语法的字符串
-		    //转换成JS对应的值
-//		    let object = window.JSON.parse(string)
-//		    console.log(typeof object)
-//		    console.log(object)
-//		    console.log('object.note')
-//		    console.log(object.note)
-//		    console.log('object.note.from')
-//		    console.log(object.note.from)
-//	    }else if(request.status >= 400){
-//	            console.log('说明请求失败')
-//	    }
-//         }
-//	}
-//	request.open('GET','/xxx')   //配置request的请求类型及路径
-//	request.send()	
-//    })
-
-
-
-
-
-
-
-//封装后的AJAX请求与响应
+//按Promise规范封装后的AJAX请求与响应
 
 window.jQuery = function(){}
 
 
 window.$ = window.jQuery
 
-window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
-		
-	let request = new XMLHttpRequest()
-	request.open(method, url)
-	for(let key in headers){
-		let value = headers[key]
-		console.log(key,value)
-		request.setRequestHeader(key,value)
+window.Promise = function(fn){
+	//...
+	return {
+		then: function(){}
 	}
+}
 
-	request.onreadystatechange = function(){
-	    if(request.readyState === 4){
-                if(request.status >= 200 && request.status < 300){
-	           successFn.call(undefined, request.responseText)
-	    }else if(request.status >= 400){
-	           failFn.call(undefined, request)
-	    }
-	    
-	  }
-	}
-        request.send(body)
+window.jQuery.ajax = function({url, method, body, headers}){
+	return new Promise(function(resolve, reject){
+		let request = new XMLHttpRequest()
+		request.open(method, url)
+		for(let key in headers){
+			let value = headers[key]
+			console.log(key,value)
+			request.setRequestHeader(key,value)
+		}
+
+		request.onreadystatechange = function(){
+			if(request.readyState === 4){
+				if(request.status >= 200 && request.status < 300){
+					resolve.call(undefined, request.responseText)
+				}else if(request.status >= 400){
+					reject.call(undefined, request)
+				}
+			
+			}
+		}
+		request.send(body)
+	})	
 }
 
 
 myButton.addEventListener('click', (e)=>{
-     window.jQuery.ajax({
-     	url:'/xxx',
-	method:'get',
-	headers:{
-		'content-type':'application/x-www-form-urlencoded',
-		'frank':'18'
-	},
-	successFn: function(x){console.log(x)},
-	failFn:function(x){console.log(x)}     
-     })
+   let Promise =  window.jQuery.ajax({
+		url:'/xxx',
+		method:'get',
+		headers:{
+			'content-type':'application/x-www-form-urlencoded',
+			'frank':'18'
+		}  
+	})
+	Promise.then(
+		function(responseText){console.log(responseText)},
+		function(request){console.log(request)}
+	)
 
 })
 
@@ -88,7 +60,7 @@ myButton.addEventListener('click', (e)=>{
 
 
 
-//引入jQuery库，按Promise规范封装
+//引入jQuery库，按Promise规范写AJAX
 
 //function success(responseText){
 //	console.log(responseText)
